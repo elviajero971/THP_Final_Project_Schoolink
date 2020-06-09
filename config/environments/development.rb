8a051dca1d674ce263fa40569716f8bfde5e32b4
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-06-09 22:27:35 +0200

    forms registration and passwords

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,8 +59,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # config.action_mailer.delivery_method = :letter_opener
   
   config.action_mailer.perform_deliveries = true
