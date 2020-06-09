aae0ce6a7a6a1d4e4de9f6524a47dd0a94932cd1
ZeddBox <jul33anc@gmail.com>
2020-06-09 14:44:59 +0200

    fix : Subject column diffuclty integer > string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_095057) do
+ActiveRecord::Schema.define(version: 2020_06_09_124120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,10 +82,10 @@ ActiveRecord::Schema.define(version: 2020_06_09_095057) do
   create_table "subjects", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "difficulty"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "difficulty"
     t.index ["user_id"], name: "index_subjects_on_user_id"
   end
 
