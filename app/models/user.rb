7418888422888286b830a23f17b06b181e6cb6cb
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-06-09 10:56:24 +0200

    feat: action mailer production

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
