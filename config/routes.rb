fe29b0ee83403ea67a07f92f2b1b709bf31c95ac
ZeddBox <jul33anc@gmail.com>
2020-06-09 14:37:17 +0200

    doc: routes and user validates

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'static#home'
   
   devise_for :users
+  
   resources :subjects
   resources :users, only: [:show] do 
     resources :profile_pics, only: [:create]
-  end  
+  end
 
 end
