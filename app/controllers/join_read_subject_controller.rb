3f54965eda59898d308307e14c6b902bf9639e50
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-06-11 23:16:57 +0200

    style(showSubject): ajax btn state read and validate

--- app/controllers/join_read_subject_controller.rb
+++ app/controllers/join_read_subject_controller.rb
@@ -3,11 +3,21 @@ class JoinReadSubjectController < ApplicationController
   def update
     @subject = Subject.find_by(id: params[:id])
     new_read = JoinReadSubject.create(subject_id: @subject.id, user_id:current_user.id)
+
+    respond_to do |format|
+      format.html { }
+      format.js { }
+    end
   end
 
   def destroy
     @subject = Subject.find_by(id: params[:id])
     read_to_destroy = JoinReadSubject.find_by(subject_id: @subject.id, user_id: current_user.id).destroy
+
+    respond_to do |format|
+      format.html { }
+      format.js { }
+    end
   end
 
 end
--- app/controllers/join_validate_subject_controller.rb
+++ app/controllers/join_validate_subject_controller.rb
@@ -3,11 +3,21 @@ class JoinValidateSubjectController < ApplicationController
   def update
     @subject = Subject.find_by(id: params[:id])
     new_val = JoinValidateSubject.create(subject_id: @subject.id, user_id:current_user.id)
+
+    respond_to do |format|
+      format.html { }
+      format.js { }
+    end
   end
 
   def destroy
     @subject = Subject.find_by(id: params[:id])
     validate_to_destroy = JoinValidateSubject.find_by(subject_id: @subject.id, user_id: current_user.id).destroy
+
+    respond_to do |format|
+      format.html { }
+      format.js { }
+    end
   end
 
 end
