f55b767a316319d370a50b8f237442a68a7f471f
ZeddBox <jul33anc@gmail.com>
2020-06-11 19:09:11 +0200

    feat : new view to user#show

--- app/controllers/join_fav_subject_controller.rb
+++ app/controllers/join_fav_subject_controller.rb
@@ -1,12 +1,12 @@
 class JoinFavSubjectController < ApplicationController
 
   def update
-    @subject = Subject.find_by(id: params[:id])
+    @subject = Subject.friendly.find(params[:id])
     new_fav = JoinFavSubject.create(subject_id: @subject.id, user_id:current_user.id)
   end
 
   def destroy
-    @subject = Subject.find_by(id: params[:id])
+    @subject = Subject.friendly.find(params[:id])
     fav_to_destroy = JoinFavSubject.find_by(subject_id: @subject.id, user_id: current_user.id).destroy
   end
 
--- app/controllers/join_validate_subject_controller.rb
+++ app/controllers/join_validate_subject_controller.rb
@@ -1,12 +1,12 @@
 class JoinValidateSubjectController < ApplicationController
 
   def update
-    @subject = Subject.find_by(id: params[:id])
+    @subject = Subject.friendly.find(params[:id])
     new_val = JoinValidateSubject.create(subject_id: @subject.id, user_id:current_user.id)
   end
 
   def destroy
-    @subject = Subject.find_by(id: params[:id])
+    @subject = Subject.friendly.find(params[:id])
     validate_to_destroy = JoinValidateSubject.find_by(subject_id: @subject.id, user_id: current_user.id).destroy
   end
 
